288f94092a636cd33ce73a8f245f1a09a826b292
Robert L. Carpenter <robacarp@gmail.com>
2013-04-09 18:18:30 -0600

    Start a place where stream test cases can be built

--- aprs/aprs_is_message_test.rb
+++ aprs/aprs_is_message_test.rb
@@ -1,4 +1,6 @@
 require 'minitest/autorun'
+require 'crack/json'
+require 'debugger'
 
 require './aprs_is_message'
 
@@ -7,21 +9,34 @@ class AprsIsMessageTest < MiniTest::Unit::TestCase
     @aim = AprsIsMessage.new
   end
 
-  def test_time_parser
-    @aim.parse_time('291651z')
+  #def test_time_parser
+  #  @aim.parse_time('291651z')
 
-    assert_equal @aim.time.hour, 16
-    assert_equal @aim.time.day, 29
-    assert_equal @aim.time.min, 51
-  end
+  #  assert_equal @aim.time.hour, 16
+  #  assert_equal @aim.time.day, 29
+  #  assert_equal @aim.time.min, 51
+  #end
 
-  def test_coordinate_parser
-    @aim.parse_coords('5209.97N/00709.65W')
-    assert @aim.lat == 5209.97
-    assert @aim.lon == -709.65
+  #def test_coordinate_parser
+  #  @aim.parse_coords('5209.97N/00709.65W')
+  #  assert @aim.lat == 5209.97
+  #  assert @aim.lon == -709.65
 
-    @aim.parse_coords('5209.97S/00709.65E')
-    assert @aim.lat == -5209.97
-    assert @aim.lon == 709.65
+  #  @aim.parse_coords('5209.97S/00709.65E')
+  #  assert @aim.lat == -5209.97
+  #  assert @aim.lon == 709.65
+  #end
+
+  def test_parsing_messages
+    @test_cases = Crack::JSON.parse( File.read( 'test_data.json' ) )
+
+    @test_cases.each do |data|
+      @aim.parse data['string']
+
+      assert_equal data['route'], @aim.route
+
+      puts "Data:   #{data['string']}"
+      puts "Sender: #{@aim.sender}"
+    end
   end
 end
