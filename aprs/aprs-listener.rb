a6eebcf191edc7d5477b92cb2700aba9344032c8
Robert L. Carpenter <robacarp@gmail.com>
2013-04-11 17:08:35 -0600

    Update the listener to match the code.

--- aprs/aprs-listener.rb
+++ aprs/aprs-listener.rb
@@ -2,16 +2,42 @@ require 'socket'
 require './aprs_is_message'
 
 def parse_line line
+  return if line[0] == '#'
   puts "PACKET: #{line}"
-  message = AprsIsMessage.parse line
-  return if message.nil?
 
-  puts "SENDER  : \033[31m#{message.sender}\033[0m"
-  puts "DEST    : \033[33m#{message.dest}\033[0m"
-  puts "PATH    : \033[37m#{message.route}\033[0m"
-  puts "DATA    : #{message.raw_data}"
-  puts "TIME    : #{message.time}"
-  puts "LAT/LON : #{message.lat}/#{message.lon}"
+  begin
+    @aim = AprsIsMessage.parse line
+  rescue
+    puts "Message Parsing failed"
+    return
+  end
+
+  return if @aim.nil?
+
+  puts "from: "  + @aim[:from]
+  puts "to: "    + @aim[:to]
+  puts "route: " + @aim[:route]
+
+  puts "lat: "  + @aim[:lat].to_s
+  puts "long: "  + @aim[:long].to_s
+
+  puts "time: "  + @aim.hour.to_s + ':' + @aim.min.to_s if @aim.has? :time
+
+  puts "course: "      + @aim[:course].to_s        if @aim.has? :course
+  puts "speed: "       + @aim[:speed].to_s         if @aim.has? :speed
+
+  puts "power: "       + @aim[:power].to_s         if @aim.has? :power
+  puts "height: "      + @aim[:height].to_s        if @aim.has? :height
+  puts "gain: "        + @aim[:gain].to_s          if @aim.has? :gain
+  puts "directivity: " + @aim[:directivity].to_s   if @aim.has? :directivity
+
+  puts "temperature: " + @aim[:temperature].to_s   if @aim.has? :temperature
+  puts "hour_rain: "   + @aim[:hour_rain].to_s     if @aim.has? :hour_rain
+  puts "day_rain: "    + @aim[:day_rain].to_s      if @aim.has? :day_rain
+  puts "todays_rain: " + @aim[:todays_rain].to_s   if @aim.has? :todays_rain
+  puts "humidity: "    + @aim[:humidity].to_s      if @aim.has? :humidity
+  puts "barometer: "   + @aim[:barometer].to_s     if @aim.has? :barometer
+  puts "\n-----------\ncomment: "     + @aim[:comment]            if @aim.has? :comment
   puts "\n-----------"
 end
 
